a849a14d4c4b17a49e95923226a431a8be79d852
Christian Simon <simon@swine.de>
2015-03-23 13:13:59 +0100

    Implemented initialization phase

--- jenkins_docker_wrapper.go
+++ jenkins_docker_wrapper.go
@@ -6,6 +6,8 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/simonswine/docker_wrapper"
 	"gopkg.in/alecthomas/kingpin.v1"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -22,14 +24,17 @@ type Arguments struct {
 type Config struct {
 	my_args            []string // Arguments for me
 	container_args     []string // Arguments for the container shell
-	files_to_copy      []string // Files to copy to the container
+	tmp_files_to_move  []string // Files to copy to the container
 	basename           string   // Base name of executable
-	secure_path        []string // Secure location to mount
+	environment        []string // Environment variables for the container
+	volumes            []string // Secure location to mount
 	default_shell      string
 	job_name           string
 	build_id           int
 	jenkins_user       string
-	jenkins_workspace  string
+	jenkins_home       string
+	workspace_path     string
+	tmp_dir            string                          // Container tmp dir
 	cleanup_containers []string                        // Containers to remove at the end
 	wrappers           *[]docker_wrapper.DockerWrapper // Docker wrappers
 }
@@ -153,10 +158,13 @@ func build_environment_validate_workspace(key string, value string) (additional 
 		return []string{}, err
 	}
 
-	if !strings.HasPrefix(path, config.jenkins_workspace) {
-		err := errors.New(fmt.Sprintf("Invalid path in %s '%s', expected to be within %s", key, path, config.jenkins_workspace))
+	workspace_path := filepath.Join(config.jenkins_home, "workspace")
+	if !strings.HasPrefix(path, workspace_path) {
+		err := errors.New(fmt.Sprintf("Invalid path in %s '%s', expected to be within %s", key, path, workspace_path))
 		return []string{}, err
 	}
+
+	config.workspace_path = path
 
 	return []string{fmt.Sprintf("%s=%s", key, path)}, err
 }
@@ -169,7 +177,7 @@ func build_environment_validate_ssh_auth_sock(key string, value string) (additio
 	}
 
 	// append ssh socket to copy slice
-	config.files_to_copy = append(config.files_to_copy, value)
+	config.tmp_files_to_move = append(config.tmp_files_to_move, value)
 
 	return []string{fmt.Sprintf("%s=%s", key, value)}, err
 }
@@ -201,10 +209,10 @@ func build_environment(env []string) (output []string, err error) {
 	m["SSH_CLIENT"] = build_environment_blacklist
 	m["SSH_CONNECTION"] = build_environment_blacklist
 	m["LD_LIBRARY_PATH"] = build_environment_blacklist
+	m["PATH"] = build_environment_blacklist
 
 	// validations
 	m["USER"] = build_environment_validate_user
-	m["PWD"] = build_environment_validate_workspace
 	m["WORKSPACE"] = build_environment_validate_workspace
 
 	// validate and move into container
@@ -250,8 +258,67 @@ func parse_config() {
 
 }
 
+func create_tmp_dir() error {
+	// TODO: Cleanup container tempdir
+	temp_dir, err := ioutil.TempDir("", "jenkins_docker_wrapper")
+	if err != nil {
+		return err
+	}
+	config.tmp_dir = temp_dir
+	log.Debugf("Created container temp dir in %s", temp_dir)
+
+	err = os.Chmod(temp_dir, 0777)
+	if err != nil {
+		return err
+	}
+
+	for i := range config.tmp_files_to_move {
+		src := config.tmp_files_to_move[i]
+		dest := filepath.Join(temp_dir, filepath.Base(src))
+		log.Debugf("Moving %s to %s", src, dest)
+		err := os.Rename(src, dest)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func copy_file(src, dst string) (int64, error) {
+	src_file, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer src_file.Close()
+
+	src_file_stat, err := src_file.Stat()
+	if err != nil {
+		return 0, err
+	}
+
+	if !src_file_stat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	dst_file, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer dst_file.Close()
+	return io.Copy(dst_file, src_file)
+}
+
+func copy_ssh_known_hosts() error {
+	source := filepath.Join(os.Getenv("HOME"), ".ssh/known_hosts")
+	dest := filepath.Join(config.tmp_dir, "known_hosts")
+	log.Debugf("Copy ssh known hosts from '%s' to '%s'", source, dest)
+	_, err := copy_file(source, dest)
+	return err
+}
+
 // set default config
-func initialize() {
+func initialize() error {
 
 	// set log level
 	log.SetLevel(log.DebugLevel)
@@ -259,13 +326,56 @@ func initialize() {
 	// set default shell to bash
 	config.default_shell = "/bin/bash"
 	config.jenkins_user = "jenkins"
-	config.jenkins_workspace = "/jenkins/workspace"
+	config.jenkins_home = "/jenkins"
 
 	config.cleanup_containers = []string{}
 
 	parse_arguments(os.Args)
 
-	build_environment(os.Environ())
+	// evaluate environment
+	env, err := build_environment(os.Environ())
+	if err != nil {
+		return err
+	}
+	config.environment = env
+
+	// add workspace to volumes
+	config.volumes = append(
+		config.volumes,
+		fmt.Sprintf(
+			"%s:%s",
+			config.workspace_path,
+			config.workspace_path,
+		),
+	)
+
+	// append script to the files to copy
+	if n := len(config.container_args); n > 0 {
+		config.tmp_files_to_move = append(config.tmp_files_to_move, config.container_args[n-1])
+	}
+
+	// create containers temp dir
+	err = create_tmp_dir()
+	if err != nil {
+		return err
+	}
+
+	// add tmp dir to volumes
+	config.volumes = append(
+		config.volumes,
+		fmt.Sprintf(
+			"%s:/tmp",
+			config.tmp_dir,
+		),
+	)
+
+	// move ssh known hosts into container
+	err = copy_ssh_known_hosts()
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 }
 
@@ -273,7 +383,10 @@ func initialize() {
 func main() {
 	defer cleanup()
 
-	initialize()
+	err := initialize()
+	if err != nil {
+		log.Panic(err)
+	}
 
 	dw, err := docker_wrapper.New()
 	if err != nil {
@@ -281,13 +394,75 @@ func main() {
 	}
 
 	dw.ImageName = *args.image_name
+	dw.Volumes = config.volumes
+	dw.Environment = config.environment
+	dw.WorkingDir = config.workspace_path
 	// Starting the docker container
 	err = dw.Run()
 	if err != nil {
 		log.Fatalf("Docker error: %s", err)
 	}
 
-	stdout, stderr, ret_val, _ := dw.RunCommand([]string{"groupadd", "-g", "1000", "jenkins"})
-	stdout, stderr, ret_val, _ = dw.RunCommand([]string{"useradd", "-d", "/jenkins", "-g", "1000", "-u", "1000", "jenkins"})
-	fmt.Printf("stdout=%s stderr=%s ret_val=%d", stdout, stderr, ret_val)
+	jenkins_home_path := "/jenkins"
+	ssh_dir_path := filepath.Join(jenkins_home_path, ".ssh")
+	ssh_known_hosts_path := filepath.Join(ssh_dir_path, "known_hosts")
+
+	gid := 1000
+	uid := 1000
+	user := "jenkins"
+	group := "jenkins"
+
+	user_group := fmt.Sprintf("%s:%s", user, group)
+	uid_str := fmt.Sprintf("%d", uid)
+	gid_str := fmt.Sprintf("%d", gid)
+
+	// remove existing user
+	stdout, stderr, ret_val, _ := dw.RunCommand([]string{"getent", "passwd", gid_str})
+	fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+	if ret_val == 0 {
+		lines := strings.Split(strings.TrimSpace(stdout), "\n")
+		for i := range lines {
+			user := strings.Split(lines[i], ":")
+			log.Debugf("Remove user '%s'", user[0])
+			stdout, stderr, ret_val, _ = dw.RunCommand([]string{"userdel", user[0]})
+			fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+		}
+	}
+	// remove existing group
+	stdout, stderr, ret_val, _ = dw.RunCommand([]string{"getent", "group", gid_str})
+	fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+	if ret_val == 0 {
+		lines := strings.Split(strings.TrimSpace(stdout), "\n")
+		for i := range lines {
+			group := strings.Split(lines[i], ":")
+			log.Debugf("Remove group '%s'", group[0])
+			stdout, stderr, ret_val, _ = dw.RunCommand([]string{"groupdel", group[0]})
+			fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+		}
+	}
+
+	stdout, stderr, ret_val, _ = dw.RunCommand([]string{"groupadd", "-g", gid_str, group})
+	fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+
+	stdout, stderr, ret_val, _ = dw.RunCommand([]string{"useradd", "-d", jenkins_home_path, "-g", gid_str, "-u", uid_str, user})
+	fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+
+	// resets sudoers file
+	stdout, stderr, ret_val, _ = dw.RunCommand([]string{"sh", "-c", "echo \"root ALL=(ALL:ALL) ALL\" > /etc/sudoers"})
+	fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+
+	// move sudoers file
+	stdout, stderr, ret_val, _ = dw.RunCommand([]string{"mkdir", "-p", ssh_dir_path})
+	fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+	stdout, stderr, ret_val, _ = dw.RunCommand([]string{"mv", "/tmp/known_hosts", ssh_known_hosts_path})
+	fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+	stdout, stderr, ret_val, _ = dw.RunCommand([]string{"chown", "-R", user_group, ssh_dir_path})
+	fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+
+	// TODO: add variables
+	command := []string{"sudo", "-E", "-u", "jenkins", "bash"}
+	command = append(command, config.container_args...)
+	dw.RunCommandAttach(command, false)
+	//fmt.Printf("stdout=%s stderr=%s ret_val=%d\n", stdout, stderr, ret_val)
+
 }
